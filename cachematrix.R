baaf0ddc35edbd3280db00d9c94816c0b172adf5
Thira <44568415+SithiraChhit@users.noreply.github.com>
2020-07-05 14:17:47 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculation of matrix inversion can be time consuming
+## Below function is to catch a matrix and calculate its matrix inversion
 
-## Write a short comment describing this function
+## This function is set to create a special 'matrix' object that can be catch its inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+inv <- NULL
+set <- function(y) {
+        x <<- y
+        inv <<- NULL
+}
+get <- function() x {
+        setInverse <- function(inverse) inv <<- inverse
+        getInverse <- function() inv
+        list(
+                set = set,
+                get = get,
+                setInverse = setInverse,
+                getInverse = getInverse
+        )
+}
 }
 
 
